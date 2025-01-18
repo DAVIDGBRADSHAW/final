486317db3fb21091c7fae54ef09239c68d565223
davidgbradshaw <davidgbradshaw@gmail.com>
2025-01-18 18:01:24 +0000

    read

---
+++ read.bat
@@ -0,0 +1,110 @@
+
+VS USED BY ME AND  EXT  DOWNLOADS
+Python -m venv .venv
+venv\Scripts\activate   on very pip dir eg root  .gives a invisible so I in my file is .venv\Scripts\activate   
+Django-admin startproject  -I picked ablog
+Cd  the newproject to get to the newly formed  file
+pip install python
+python.exe -m pip install –upgrade pip
+pip install Django
+python manage.py makemigrations ( every time there is big amend)
+python manage.py migrate
+python manage.py runserver – follow link
+python manage.py createsuperuser
+then python manage.py runserver again to update the system
+python  manage.py startapp theblog
+if u want another foder do the same python manage.py startapp  members
+pip freeze
+pip freeze > requirements.txt
+pip install -r requirements.txt
+Pip install mysql-connector
+Pip install mysql-connector-python
+Pip install mysql-connector-python-rf
+Python manage.py shell
+Pip install crispy form
+Ls
+PIL is python image library   . put pip install pillow
+pip install -r requirements.txt 
+python or py or python3 manage.py migrate –_run-syncdb
+Pip install Django restframework
+Django-admin startproject newproject
+Cd newproject/
+Python manage.py startapp api
+Python  manage.py shell
+Python -m pip install Django-cors-headers
+Pip install virtualenv
+Python manage.py makemigrations
+Python manage.py migrate
+Ctrl c to close venv
+Python manage.py makemigration  -  at the start will be NO CHANCES DETECTED
+Python manage.py runserver
+You get http://127.0.0.1:8000/
+Control C 
+Python manage.py createsuperuser
+
+     PRETTIER added to extension
+ATOM ONE DARK THEME
+API – application programming interface
+JSON – java script  object notation
+ REST - (REpresentational State Transfer) is an architectural style for developing web services and systems that can easily communicate with each other.
+POST C
+GET R
+PUT UPDATE U
+Delete D
+Idempotent
+ORM
+GUI – Graphical User Interface
+pip freeze
+pip freeze > requirements.txt
+pip install -r requirements.txt
+
+cd ..  changes back a directory
+Cd desktop ( change directory)
+File manager  cmd this opens  CommandPropmt
+
+API  - application programming interface
+URL  - universal resource locator
+URI – universal resource identifier
+http  HyperText Transfer Protocol
+Hyper Text Markup Language
+404 vs 200 which is ok
+REST - Represential State Transfer
+Pip is a recursive acronym for ‘Pip Installs Packages.
+---
+Client-Server Architecture - HTTP
+Statelessness  - HTTP
+Layered system
+Cacheability
+Uniform design
+Code on demand
+
+---
+JSON  JavaScript Object Notation
+
+===
+HTTP VERBS / CRUD
+GET  / read
+POST / create
+PUT / PATCH update
+DELETE /delete
+Cross – cross origin resourse sharing
+Python -m pip install Django-cors-headers
+Cd = change directory
+Cd ..  goes back one directory
+ Cd  ablog goes forward  or what every u call ur file
+--
+3 step process  - url view,html
+CROSS SITE  REQUEST FOGERTY TOKEN  csrf_token
+Args = agreements
+Ico = icon
+
+Python quickstart  |  Google Calendar  |  Google for Developers
+Pip = python . . program
+Mk = make
+Dir  = directory
+Mkdir make
+
+
+MVT = MODELS
+		VIEWS
+			TEMPLATES
